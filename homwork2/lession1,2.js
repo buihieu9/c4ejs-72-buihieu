ee111ba962a9237f1df304d81870f1a2a94856c4
enter <buivanhieu1871997@gmail.com>
2020-02-19 02:50:59 +0700

    home

--- homwork2/lession1,2.js
+++ homwork2/lession1,2.js
@@ -5,7 +5,16 @@ let c = a ;
 a = b;
 b = c ;
 console.log('a ='+ a +' b ='+b)
- 
+
+//2 
+
+    let a = 5;
+    let b = 6;
+    a = a^b;
+    b = a^b;
+    a = a^b;
+    console.log(a,b)
+
 // lession2
 const s = 'Hello beauty there';
 const a = s.split(' ',3)
